72ba870012aa0fd9c575aa5b2519c3daf6c434d8
samuelige <samuelige.iyanu@gmail.com>
2021-06-04 20:17:39 +0100

    Added Header Component

---
+++ src/Route/Route.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../HOC/Layout'
+
+const Route = () => {
+    return (
+        <Layout>
+            <h1>Hello, Sammy</h1>
+        </Layout>
+    )
+}
+
+export default Route
